06cf98624e3fe2b3f77bf9970e8868fb4a6d42ea
samratmoin <virus.moin@gmail.com>
2021-08-25 13:05:16 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        banner_bg: {
+          400: "#FFFBF0",
+        },
+        li_color: {
+          400: "#3C3C3C",
+        },
+        banner_pri: {
+          400: "#FABE4C",
+        },
+        banner_sec: {
+          400: "#363958",
+        },
+        penguin_primary: {
+          400: "#A4BC46",
+        },
+        penguin_secondary: {
+          400: "#85A019",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
